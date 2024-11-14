42026fcacb950ddac1c8703473a75abe2a92e2dc
aploe <andre.ploeger@mi-bochum.de>
2024-11-14 11:43:16 +0100

    Added necessary fonts from google and installation via pip

--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
 from setuptools import setup
+from setuptools.command.install import install
+import os
+import shutil
+import pkg_resources
 
+class PostInstallCommand(install):
+    def run(self):
+        # Run the standard install first
+        install.run(self)
+        
+        # Create user fonts directory if it doesn't exist
+        user_fonts_dir = os.path.expanduser('~/.local/share/fonts')
+        os.makedirs(user_fonts_dir, exist_ok=True)
+        
+        # Copy fonts from package to user fonts directory
+        try:
+            font_files = pkg_resources.resource_listdir('mtglabels', 'fonts')
+            for font_file in font_files:
+                src = pkg_resources.resource_filename('mtglabels', f'fonts/{font_file}')
+                dst = os.path.join(user_fonts_dir, font_file)
+                shutil.copy2(src, dst)
+            
+            # Update font cache
+            os.system('fc-cache -f')
+        except Exception as e:
+            print(f"Warning: Could not install fonts: {e}")
 
-setup()
+setup(
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
